a703c8dec17d7d13fe980e1610a4256c42da48ab
edwardzh976 <84339439+edwardzh976@users.noreply.github.com>
2024-09-04 15:31:07 -0400

    Project init

---
+++ quiz-app/src/components/QuizOptions.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="500"
+  >
+
+    <v-toolbar
+      color="indigo"
+      dark
+    >
+
+      <v-toolbar-title>Quizzes</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+    </v-toolbar>
+
+    <v-container fluid>
+      <v-row dense>
+        <v-col
+          v-for="card in cards"
+          :key="card.title"
+          :cols="card.flex"
+        >
+          <v-card>
+            <v-img
+              :src="card.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title v-text="card.title"></v-card-title>
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+<script>
+  export default {
+    data: () => ({
+      cards: [
+        { title: 'Math', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 6 },
+        { title: 'Science', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+        { title: 'Art', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+      ],
+    }),
+  }
+</script>
---
+++ quiz-app/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <quiz-options/>
+</div>
+  
+</template>
+
+<script>
+//import QuizCard from '../components/QuizCard'
+import QuizOptions from '../components/QuizOptions.vue'
+  export default {
+    name: 'Home-Page',
+    components: {
+      QuizOptions,
+    
+    },
+  }
+</script>
