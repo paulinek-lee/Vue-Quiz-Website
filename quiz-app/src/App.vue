9b90089f09a646571764782e492676d5bd10277b
paulinek-lee <pauline.k.lee@gmail.com>
2024-09-10 15:09:07 -0400

    score synced across multiple pages

--- quiz-app/src/App.vue
+++ quiz-app/src/App.vue
@@ -5,43 +5,34 @@
       color="#6482AD"
       dark
     >
-    <a href="/"
-    style="text-decoration: none;
-  color: inherit; /* Inherit color from parent */
-">
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <h1>IQ Odyssey</h1>
-        
-      </div>
-    </a>
+      <router-link to="/" style="text-decoration: none; color: inherit;">
+        <div class="d-flex align-center">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+          />
+          <h1>IQ Odyssey</h1>
+        </div>
+      </router-link>
 
       <v-spacer></v-spacer>
 
-      <h2 v-if="$route.path !== '/'">Current Score: {{this.$store.getters.getCount}}</h2>
+      <h2 v-if="$route.path !== '/'">Current Score: {{ this.$store.getters.getCount }}</h2>
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
-
-  data: () => ({
-    //
-  }),
+  data: () => ({}),
 };
 </script>
